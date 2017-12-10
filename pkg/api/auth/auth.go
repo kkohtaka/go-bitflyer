0c6335e4ed673bf93fb8f3dd924539b51fed41ee
Kazumasa Kohtaka <kkohtaka@gmail.com>
2017-12-10 22:40:11 +0900

    Add a function to generate authentication signature

---
+++ pkg/api/auth/auth.go
@@ -0,0 +1,46 @@
+// Copyright (C) 2017 Kazumasa Kohtaka <kkohtaka@gmail.com> All right reserved
+// This file is available under the MIT license.
+
+package auth
+
+import (
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/kkohtaka/go-bitflyer/pkg/api"
+)
+
+type AuthConfig struct {
+	APIKey    string
+	APISecret string
+}
+
+func GenerateAuthHeaders(config *AuthConfig, now time.Time, api api.API, req api.Request) *http.Header {
+	url := api.BaseURL()
+	url.RawQuery = req.Query()
+
+	timestamp := fmt.Sprintf("%d", now.Unix())
+	method := req.Method()
+	path := url.Path
+	payload := req.Payload()
+
+	mac := hmac.New(sha256.New, []byte(config.APISecret))
+	mac.Write([]byte(timestamp))
+	mac.Write([]byte(method))
+	mac.Write([]byte(path))
+	if len(payload) > 0 {
+		mac.Write(payload)
+	}
+	sign := hex.EncodeToString(mac.Sum(nil))
+
+	header := http.Header{}
+	header.Set("ACCESS-KEY", config.APIKey)
+	header.Set("ACCESS-TIMESTAMP", timestamp)
+	header.Set("ACCESS-SIGN", sign)
+
+	return &header
+}
